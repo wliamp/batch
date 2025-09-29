a2db9fb4f37554b32953e9d9be4fa791f574c804
William Phan <phnam230197@gmail.com>
2025-09-30 01:25:39 +0700

    feat(cleanup): initial module cleanup

--- cleanup/src/main/java/io/wliamp/notion/MainClass.java
+++ cleanup/src/main/java/io/wliamp/notion/MainClass.java
@@ -1,3 +1,11 @@
-void main() {
-  $END$
+package io.wliamp.notion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
 }
